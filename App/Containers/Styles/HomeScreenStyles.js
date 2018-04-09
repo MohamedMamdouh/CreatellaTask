8ca68dab8f7000641e3dcc409a073c0a5a9978de
Mohamed Mamdouh <eng_mohamed93@msn.com>
2018-04-09 08:32:02 +0200

    Home Style and App Navigation Init

---
+++ App/Containers/Styles/HomeScreenStyles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native'
+import { Metrics, ApplicationStyles } from '../../Themes/'
+
+export default StyleSheet.create({
+  ...ApplicationStyles.screen,
+  container: {
+    paddingBottom: Metrics.baseMargin,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1
+  },
+  logo: {
+    marginTop: Metrics.doubleSection,
+    height: Metrics.images.logo,
+    width: Metrics.images.logo,
+    resizeMode: 'contain'
+  },
+  centered: {
+    alignItems: 'center'
+  },
+  contentContainer: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  }
+})
